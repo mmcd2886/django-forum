168555b26154fa96e4c82d4544738aa75154805f
mmcd2886 <61208690+mmcd2886@users.noreply.github.com>
2021-07-29 11:51:57 -0400

    Update models.py

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -38,6 +38,7 @@ class Posts(models.Model):
 
     # you can only select one primary key in a Django model. UniqueConstraint allows you to set
     # two unique values (works like a composite key). A user cannot post at the same date_time, so this will be unique
+    # and prevent a the same user reply from being stored in the DB more than once.
     class Meta:
         constraints = [models.UniqueConstraint(fields=['username', 'date_time'], name='composite_key')]
 
