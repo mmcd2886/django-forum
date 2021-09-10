976611e8999e9dcf7977fe2ce12189d9e5caef24
mmcd2886 <61208690+mmcd2886@users.noreply.github.com>
2021-09-10 09:39:35 -0400

    Update tests.py

--- mysite/forum/tests.py
+++ mysite/forum/tests.py
@@ -9,7 +9,7 @@ from .models import Threads
 class ForumModelTests(TestCase):
     def test_was_created_recently_with_future_thread(self):
         """ was_created_recently() returns False for threads whose created date
-        is in the future. """
+        are in the future. """
         time = timezone.now() + datetime.timedelta(days=30)
         future_thread = Threads(date_time=time)
         self.assertIs(future_thread.was_created_recently(), False)
